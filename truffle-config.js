417a74219477ec1cae35595922a343b74ebf53e2
dulcedu <dulcedu@gmail.com>
2020-04-29 22:38:16 -0700

    step: 04-03: read gas prices and BIP39 mnemonic in from files

--- truffle-config.js
+++ truffle-config.js
@@ -1,3 +1,29 @@
+const HDWalletProvider = require('@truffle/hdwallet-provider');
+const fs = require('fs');
+
+const gasPriceTestnetRaw = fs.readFileSync(".gas-price-testnet.json").toString().trim();
+const gasPriceTestnet = parseInt(JSON.parse(gasPriceTestnetRaw).result, 16);
+if (typeof gasPriceTestnet !== 'number' || isNaN(gasPriceTestnet)) {
+  throw new Error('unable to retrieve network gas price from .gas-price-testnet.json');
+}
+const gasPriceRegtestRaw = fs.readFileSync(".gas-price-regtest.json").toString().trim();
+const gasPriceRegtest = parseInt(JSON.parse(gasPriceRegtestRaw).result, 16);
+console.log(gasPriceRegtest);
+if (typeof gasPriceRegtest !== 'number' || isNaN(gasPriceRegtest)) {
+  throw new Error('unable to retrieve network gas price from .gas-price-regtest.json');
+}
+const mnemonic = fs.readFileSync(".secret").toString().trim();
+if (!mnemonic || mnemonic.split(' ').length !== 12) {
+  throw new Error('unable to retrieve mnemonic from .mnemonic');
+}
+console.log({
+  mnemonic,
+  gasPriceTestnet,
+  gasPriceRegtest,
+});
+// NOTE only do the above in demo code.
+// This is not, by far, secure enough for a real use scenario.
+
 module.exports = {
   // See <http://truffleframework.com/docs/advanced/configuration>
   // for more about customizing your Truffle configuration!
