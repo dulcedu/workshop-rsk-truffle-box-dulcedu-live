85154ec944345d3c793c191d42c88452a7b44650
dulcedu <dulcedu@gmail.com>
2020-04-29 23:06:26 -0700

    step: 06-02: test for adopt function

--- test/adoption.test.js
+++ test/adoption.test.js
@@ -1,6 +1,31 @@
 const Adoption = artifacts.require('Adoption');
 
+const assert = require('assert');
+
+const BN = web3.utils.BN;
+
+
 contract('Adoption', (accounts) => {
+    it('account can adopt pet', async () => {
+        const inst = await Adoption.deployed();
+    
+        const adopterAccount = accounts[5];
+        const expectedPetId = new BN(8);
+        const adoptTxInfo = await inst.adopt(
+          expectedPetId,
+          {
+            from: adopterAccount,
+          },
+        );
+    
+        assert.equal(
+          adoptTxInfo.receipt.status,
+          true,
+          'adoption transaction failed',
+        );
+      });
+    
+    
 
 });
 
